31309255bd6746c279a49eb4dd79bfee6fc462ca
Roberto Junior <139507352+RobertoRjunior@users.noreply.github.com>
2024-07-26 10:44:01 -0300

    Update ListaProdutosServlet.java

--- TP02 SWII5/src/main/java/produto/ListaProdutosServlet.java
+++ TP02 SWII5/src/main/java/produto/ListaProdutosServlet.java
@@ -1,6 +1,5 @@
-
+//Roberto Junior; Mariana C. da Luz
 package produto;
-
 import java.io.IOException;
 import java.util.List;
 
@@ -10,7 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//Roberto Junior; Mariana C. da Luz
+
 @WebServlet("/ListaProdutos")
 public class ListaProdutosServlet extends HttpServlet{
 
